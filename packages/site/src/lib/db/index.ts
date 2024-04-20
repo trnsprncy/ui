1a9c87544f2b5388c74c088d3e2eff77d1edeee6
trnsprncy <transparencyui@gmail.com>
2024-04-20 02:28:58 -0400

    fix: import errors

--- packages/site/src/lib/db/index.ts
+++ packages/site/src/lib/db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from 'drizzle-orm/libsql';
+import { env } from "@/env.mjs";
 import { createClient } from "@libsql/client";
-import { env } from "@/lib/env.mjs";
- 
+import { drizzle } from "drizzle-orm/libsql";
+
 export const sqlite = createClient({
   url: env.DATABASE_URL,
   authToken: env.DATABASE_AUTH_TOKEN,
--- packages/site/src/lib/email/index.ts
+++ packages/site/src/lib/email/index.ts
@@ -1,4 +1,4 @@
+import { env } from "@/env.mjs";
 import { Resend } from "resend";
-import { env } from "@/lib/env.mjs";
 
 export const resend = new Resend(env.RESEND_API_KEY);
