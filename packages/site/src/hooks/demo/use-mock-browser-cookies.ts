4bcf21070c5e67fbec6ec11f735492b778b37233
trnsprncy <transparencyui@gmail.com>
2024-04-25 16:29:32 -0400

    feat: add mock hook for demo cookie mgmt

---
+++ packages/site/src/hooks/demo/use-mock-browser-cookies.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function useMockBrowserCookies() {
+  const [cookies, setCookies] = useState<{ [name: string]: any }>({});
+
+  const getCookie = (name: string) => {
+    return cookies[name];
+  };
+
+  const setCookie = (name: string, value: any, options: any = {}) => {
+    let cookie = {
+      name,
+      value,
+      ...options,
+    };
+    setCookies({
+      ...cookies,
+      [name]: cookie,
+    });
+  };
+
+  const removeCookie = (name: string) => {
+    let updatedCookies = { ...cookies };
+    delete updatedCookies[name];
+    setCookies(updatedCookies);
+  };
+
+  return {
+    getCookie,
+    setCookie,
+    removeCookie,
+  };
+}
