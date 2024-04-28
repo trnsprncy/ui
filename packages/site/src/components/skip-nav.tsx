2962cc0bb08c810e8ad216c6fc8c85f8c1ac71fd
trnsprncy <transparencyui@gmail.com>
2024-04-27 22:26:00 -0400

    fix: skipnav styles

--- packages/site/src/components/skip-nav.tsx
+++ packages/site/src/components/skip-nav.tsx
@@ -7,7 +7,7 @@ export const SkipNav = ({ href }: { href?: string }) => {
   return (
     <Link
       className={cn(
-        "absolute left-0 p-3 m-3 transition -translate-y-24 focus:translate-y-4 z-20",
+        "absolute left-0 p-3 m-3 transition -translate-y-24 opacity-0 focus:translate-y-4 focus:opacity-1 z-20",
         buttonVariants({ variant: "default" })
       )}
       href={href ?? "#main-content"}
